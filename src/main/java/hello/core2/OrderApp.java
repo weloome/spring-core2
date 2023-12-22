eea115c7a0245eab8ba4f69f2ec6b5f579ef81df
danbi <leedanbiii@naver.com>
2023-12-23 00:25:45 +0900

    스프링으로 전환하기

--- src/main/java/hello/core2/OrderApp.java
+++ src/main/java/hello/core2/OrderApp.java
@@ -5,12 +5,14 @@ import hello.core2.member.Member;
 import hello.core2.member.MemberService;
 import hello.core2.order.Order;
 import hello.core2.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
