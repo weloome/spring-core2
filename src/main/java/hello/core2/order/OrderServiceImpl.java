7e330d5ff52e0ff91c0b7fac022f736fca1e0039
danbi <leedanbiii@naver.com>
2023-12-28 10:08:51 +0900

    롬복

--- src/main/java/hello/core2/order/OrderServiceImpl.java
+++ src/main/java/hello/core2/order/OrderServiceImpl.java
@@ -3,19 +3,15 @@ package hello.core2.order;
 import hello.core2.discount.DiscountPolicy;
 import hello.core2.member.Member;
 import hello.core2.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
