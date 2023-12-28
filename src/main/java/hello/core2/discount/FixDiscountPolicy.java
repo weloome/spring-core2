cf5f19fd79f9ad7ffa741d9048668cde5f2e3f3e
danbi <leedanbiii@naver.com>
2023-12-28 10:49:45 +0900

    @Qualifier

--- src/main/java/hello/core2/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core2/discount/FixDiscountPolicy.java
@@ -2,7 +2,11 @@ package hello.core2.discount;
 
 import hello.core2.member.Grade;
 import hello.core2.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; //1000원 할인
--- src/main/java/hello/core2/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core2/discount/RateDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.core2.discount;
 
 import hello.core2.member.Grade;
 import hello.core2.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("mainDiscountPolicy")
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10; //10% 할인
