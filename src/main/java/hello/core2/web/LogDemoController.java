24592e2dc8c2e4417d3ff9d16d6d7714daf8fd96
danbi <leedanbiii@naver.com>
2023-12-28 15:37:20 +0900

    스코프와 프록시

--- src/main/java/hello/core2/web/LogDemoController.java
+++ src/main/java/hello/core2/web/LogDemoController.java
@@ -12,13 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
